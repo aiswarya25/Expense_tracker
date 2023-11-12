ac4c966e73a38f081eac2551a59b4e4533ba6252
aiswarya25 <aiswaryapbindhu3@gmail.com>
2023-11-12 20:54:46 +0530

    Completed changes on components

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -31,7 +31,7 @@ function Transaction({ category }){
     if(!category) return null;
     return(
         <div className="item flex justify-center bg-gray-50 py-2 rounded-r" style={{borderRight:`8px solid ${category.color ?? "#e5e5e5"}`}}>
-            <button className='px-3'><box-icon name="trash"></box-icon></button>
+            <button className='px-3'><box-icon color={category.color ?? "#e5e5e5"} size="15px" name="trash"></box-icon></button>
         <span className='block w-full'>{category.name ?? ''}</span>
         </div>
     )
